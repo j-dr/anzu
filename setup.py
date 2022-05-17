ef300c29c67b4c36014824504a4ef63a5a90da1b
Joseph DeRose <joe.derose13@gmail.com>
2022-05-17 10:22:55 -0700

    restructure submodlues

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name='anzu',
     version='1.0',
     packages=find_packages(),
-    package_dir={'anzu' : 'anzu'},
-    scripts=['fields/run_fields.py'],
+    package_dir={'anzu' : 'anzu',
+                 'fields' :'fields'},
+    scripts=['bin/run_fields.py'],
     package_data={'anzu': ['data/*']},
     long_description=open('README.md').read(),
     )
